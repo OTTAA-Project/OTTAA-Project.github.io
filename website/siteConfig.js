ee1fa968cd826c62dad6a2e2b9deef265a4eb10a
ottaa-project <gonojuarez17@gmail.com>
2021-05-26 19:02:48 -0300

    analytics

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -104,5 +104,14 @@ const siteConfig = {
   // template. For example, if you need your repo's URL...
   // repoUrl: 'https://github.com/facebook/test-site',
 };
-
+module.exports = {
+  plugins: ['@docusaurus/plugin-google-analytics'],
+  themeConfig: {
+    googleAnalytics: {
+      trackingID: 'ottaa-project:analytics_150845831',
+      // Optional fields.
+      anonymizeIP: true, // Should IPs be anonymized?
+    },
+  },
+};
 module.exports = siteConfig;
