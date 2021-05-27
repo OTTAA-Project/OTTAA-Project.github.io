fbaa05a1b88711037d6bcdaa83211347044f3245
ottaa-project <gonojuarez17@gmail.com>
2021-05-27 16:51:43 -0300

    changes from the analytics

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -82,7 +82,7 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: [siteUrl + "js/ep-google-tag-manager.js",'https://buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
